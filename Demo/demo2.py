6d2166e09dbe3caf0710835c6a61e62f8739c3f3
Fatih Dağ <dagbfatih@hotmail.com>
2024-06-29 19:20:46 +0300

    Naming updates

--- Demo/demo2.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  1 15:47:25 2024
-
-@author: dagbf
-"""
-
--- athletes_data_prepration_demo/fixData.py
+++ athletes_data_prepration_demo/fixData.py
@@ -4,6 +4,7 @@ import numpy as np
 from collections import Counter
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 data = pd.read_csv("athletes.csv")
@@ -12,25 +13,29 @@ data.info()
 
 print(data.columns)
 
-data.rename(columns={
-    'ID':'Id',
-    'Name':'Name',
-    'Sex':'Gender',
-    'Age':'Age',
-    'Height':'Height',
-    'Weight':'Weight',
-    'Team':'Team',
-    'NOC':'Noc',
-    'Games':'Games',
-    'Year':'Year',
-    'Season':'Season',
-    'City':'City',
-    'Sport':'Sport',
-    'Event':'Event',
-    'Medal':'Medal',
-    }, inplace=True)
+data.rename(
+    columns={
+        "ID": "Id",
+        "Name": "Name",
+        "Sex": "Gender",
+        "Age": "Age",
+        "Height": "Height",
+        "Weight": "Weight",
+        "Team": "Team",
+        "NOC": "Noc",
+        "Games": "Games",
+        "Year": "Year",
+        "Season": "Season",
+        "City": "City",
+        "Sport": "Sport",
+        "Event": "Event",
+        "Medal": "Medal",
+    },
+    inplace=True,
+)
 
-print();print()
+print()
+print()
 print(data.head(2))
 
 data = data.drop(["Id", "Games"], axis=1)
@@ -41,33 +46,31 @@ print("Unique values: {}".format(len(uniqueEvents)))
 
 # her bir eventi iteratif olarak dolaş
 # event özelinde height ye weight ortalamalarını hesapla
-# event özelinde kayıp height ye weight değerlerini 
-# etkinlik ortalamalarına eşitle 
+# event özelinde kayıp height ye weight değerlerini
+# etkinlik ortalamalarına eşitle
 
-tempData=data.copy()
+tempData = data.copy()
 
 for event in uniqueEvents:
-    
+
     filteredEvents = tempData.Event == event
-    
+
     filteredData = tempData[filteredEvents]
-    
+
     for s in ["Height", "Weight"]:
-        average = np.round(
-            np.mean(filteredData[s]),
-            2)
-        
+        average = np.round(np.mean(filteredData[s]), 2)
+
         if ~np.isnan(average):
             filteredData[s] = filteredData[s].fillna(average)
         else:
             averageOfAllData = np.round(np.mean(data[s]), 2)
             filteredData[s] = filteredData[s].fillna(averageOfAllData)
-    
+
     tempData[filteredEvents] = filteredData
 
 data = tempData.copy()
-print(data.info())    
-        
+print(data.info())
+
 # fill the Age column nan values
 
 averageOfAges = np.round(np.mean(data.Age), 2)
@@ -86,4 +89,4 @@ data = data[filterOfAthletesHaveMedal]
 
 print(len(data))
 
-data.to_csv("athletes_clean.csv", index = False)
+data.to_csv("athletes_clean.csv", index=False)
